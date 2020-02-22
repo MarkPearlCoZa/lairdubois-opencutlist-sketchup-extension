1bcab7440beb16142b76247e7e03fad2737f1a87
markpearl <markpearl@gmail.com>
2020-02-22 15:19:00 +1300

    More asseerts

--- src/ladb_opencutlist/ruby/TestEntryPoint_spec.rb
+++ src/ladb_opencutlist/ruby/TestEntryPoint_spec.rb
@@ -31,6 +31,119 @@ module Ladb
           e.add_box(500, 500)
           result = e.run
           expect(result).is_a?(BinPacking2D)
+          expect(result[0].score).to eq 5
+          expect(result[0].saw_kerf).to eq 1
+          # expect(result[0].trimsize).to eq 1
+          expect(result[0].split).to eq 5
+          #expect(result[0].rotatable).to eq false
+          expect(result[0].original_bins).kind_of?(Array)
+          expect(result[0].original_bins.length).to eq 1
+          expect(result[0].original_bins[0].length).to eq 2000
+          expect(result[0].original_bins[0].width).to eq 1000
+          expect(result[0].original_bins[0].x).to eq 0
+          expect(result[0].original_bins[0].y).to eq 0
+
+          expect(result[0].original_bins[0].boxes).kind_of?(Array)
+          expect(result[0].original_bins[0].boxes.length).to eq 1
+          expect(result[0].original_bins[0].boxes[0].length).to eq 500
+          expect(result[0].original_bins[0].boxes[0].width).to eq 500
+          expect(result[0].original_bins[0].boxes[0].x).to eq 0
+          expect(result[0].original_bins[0].boxes[0].y).to eq 0
+          expect(result[0].original_bins[0].boxes[0].rotated).to eq false
+          expect(result[0].original_bins[0].boxes[0].data).to eq nil
+          expect(result[0].original_bins[0].boxes[0].sboxes).kind_of?(Array)
+          expect(result[0].original_bins[0].boxes[0].is_superbox).to eq false
+
+          expect(result[0].original_bins[0].cuts).kind_of?(Array)
+          expect(result[0].original_bins[0].cuts.length).to eq 4
+          expect(result[0].original_bins[0].cuts[0].x).to eq 0
+          expect(result[0].original_bins[0].cuts[0].y).to eq 500
+          expect(result[0].original_bins[0].cuts[0].length).to eq 500
+          expect(result[0].original_bins[0].cuts[0].is_primary).to eq true
+          expect(result[0].original_bins[0].cuts[0].is_horizontal).to eq true
+
+          expect(result[0].original_bins[0].cuts[1].x).to eq 500
+          expect(result[0].original_bins[0].cuts[1].y).to eq 0
+          expect(result[0].original_bins[0].cuts[1].length).to eq 500
+          expect(result[0].original_bins[0].cuts[1].is_primary).to eq true
+          expect(result[0].original_bins[0].cuts[1].is_horizontal).to eq false
+
+          expect(result[0].original_bins[0].cuts[2].x).to eq 0
+          expect(result[0].original_bins[0].cuts[2].y).to eq 500
+          expect(result[0].original_bins[0].cuts[2].length).to eq 2000
+          expect(result[0].original_bins[0].cuts[2].is_primary).to eq true
+          expect(result[0].original_bins[0].cuts[2].is_horizontal).to eq true
+
+          expect(result[0].original_bins[0].total_length_cuts).to eq 3500
+          expect(result[0].original_bins[0].efficiency).to eq 12.5
+
+          expect(result[0].original_bins[0].leftovers).kind_of?(Array)
+          expect(result[0].original_bins[0].leftovers.length).to eq 2
+          expect(result[0].original_bins[0].leftovers[0].length).to eq 2000
+          expect(result[0].original_bins[0].leftovers[0].width).to eq 499
+          expect(result[0].original_bins[0].leftovers[0].type).to eq 0
+          expect(result[0].original_bins[0].leftovers[0].x).to eq 0
+          expect(result[0].original_bins[0].leftovers[0].y).to eq 501
+          #expect(result[0].original_bins[0].leftovers[0].max_x).to eq 0
+          #expect(result[0].original_bins[0].leftovers[0].max_y).to eq 0
+          expect(result[0].original_bins[0].leftovers[0].boxes).kind_of?(Array)
+          expect(result[0].original_bins[0].leftovers[0].boxes.length).to eq 0
+          expect(result[0].original_bins[0].leftovers[0].cuts).kind_of?(Array)
+          expect(result[0].original_bins[0].leftovers[0].cuts.length).to eq 0
+          expect(result[0].original_bins[0].leftovers[0].total_length_cuts).to eq 0
+          expect(result[0].original_bins[0].leftovers[0].efficiency).to eq 0
+          expect(result[0].original_bins[0].leftovers[0].leftovers).kind_of?(Array)
+          expect(result[0].original_bins[0].leftovers[0].leftovers.length).to eq 0
+          expect(result[0].original_bins[0].leftovers[0].trimmed).to eq false
+          expect(result[0].original_bins[0].leftovers[0].trimsize).to eq 0
+          #expect(result[0].original_bins[0].leftovers[0].bbox_done).to eq true
+          #expect(result[0].original_bins[0].leftovers[0].cleaned).to eq true
+
+          expect(result[0].original_bins[0].leftovers[1].length).to eq 1499
+          expect(result[0].original_bins[0].leftovers[1].width).to eq 500
+          expect(result[0].original_bins[0].leftovers[1].type).to eq 0
+          expect(result[0].original_bins[0].leftovers[1].x).to eq 501
+          expect(result[0].original_bins[0].leftovers[1].y).to eq 0
+          #expect(result[0].original_bins[0].leftovers[1].max_x).to eq 0
+          #expect(result[0].original_bins[0].leftovers[1].max_y).to eq 0
+          expect(result[0].original_bins[0].leftovers[1].boxes).kind_of?(Array)
+          expect(result[0].original_bins[0].leftovers[1].boxes.length).to eq 0
+          expect(result[0].original_bins[0].leftovers[1].cuts).kind_of?(Array)
+          expect(result[0].original_bins[0].leftovers[1].cuts.length).to eq 0
+          expect(result[0].original_bins[0].leftovers[1].total_length_cuts).to eq 0
+          expect(result[0].original_bins[0].leftovers[1].efficiency).to eq 0
+          expect(result[0].original_bins[0].leftovers[1].leftovers).kind_of?(Array)
+          expect(result[0].original_bins[0].leftovers[1].leftovers.length).to eq 0
+          expect(result[0].original_bins[0].leftovers[1].trimmed).to eq false
+          expect(result[0].original_bins[0].leftovers[1].trimsize).to eq 0
+          #expect(result[0].original_bins[0].leftovers[1].bbox_done).to eq true
+          #expect(result[0].original_bins[0].leftovers[1].cleaned).to eq true
+
+          expect(result[0].original_bins[0].trimmed).to eq true
+          expect(result[0].original_bins[0].trimsize).to eq 0
+          #expect(result[0].original_bins[0].bbox_done).to eq false
+
+          expect(result[0].unplaced_boxes).kind_of?(Array)
+          expect(result[0].unplaced_boxes.length).to eq 0
+
+          expect(result[0].unused_bins).kind_of?(Array)
+          expect(result[0].unused_bins.length).to eq 1
+
+          #expect(result[0].original_bins[0].max_x).to eq 101
+          #expect(result[0].original_bins[0].max_y).to eq 101
+
+          #expect(result[0].b_x).to eq 0
+          #expect(result[0].b_y).to eq 0
+          #expect(result[0].b_w).to eq 1830
+          #expect(result[0].b_l).to eq 2750
+          #expect(result[0].stacking_maxlength).to eq 2000
+          #expect(result[0].stacking_maxwidth).to eq 2000
+          expect(result[0].performance.largest_leftover_length).to eq 2000
+          expect(result[0].performance.largest_leftover_width).to eq 499
+          expect(result[0].performance.nb_bins).to eq 1
+          expect(result[0].performance.nb_boxes_packed).to eq 1
+          expect(result[0].performance.nb_leftovers).to eq 2
+          #expect(result[0].performance.packing_quality).to eq 0
         end
         it "should return a double placement if there are two box that can fit in 2 bins" do
           options.base_bin_length = 0
